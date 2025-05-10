b2687669078698491f1c15611db09451c7501c5f
Muhammad <mujtabakhalid1999@gmail.com>
2025-05-10 23:24:51 +0500

    apt imgs added

--- src/app/pages/properties/components/page-2/page-2.component.ts
+++ src/app/pages/properties/components/page-2/page-2.component.ts
@@ -1,10 +1,10 @@
-import { CurrencyPipe, DatePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, DecimalPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { parseNumber } from '../../../../shared/utils/app.utils';
 
 @Component({
   selector: 'app-page-2',
-  imports: [DatePipe, CurrencyPipe],
+  imports: [DatePipe, CurrencyPipe, DecimalPipe],
   templateUrl: './page-2.component.html',
   styleUrl: './page-2.component.scss',
 })
@@ -13,6 +13,7 @@ export class Page2Component {
   installmentsPlan = input<any>();
 
   getCalculatedAdmFee(val: any) {
+    console.log(val);
     if (val) return parseNumber(val) * 0.02;
     return '';
   }
